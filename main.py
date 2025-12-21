f1d83022652b7b71c1c2599389d4e3ca541bdd4d
mikolajDeb <mikolajforaps@gmail.com>
2025-12-21 23:44:21 +0100

    getting the text

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        contents = f.read()
+    return contents
+def main():
+    text = get_book_text("/home/mikolaj/workspace/github.com/MD/bookbot/books/frankenstein.txt")
+    print (text)
+
+main()
