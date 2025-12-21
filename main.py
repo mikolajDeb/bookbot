c3e1d6d2747db55e07d688262cb74e257a43890a
mikolajDeb <mikolajforaps@gmail.com>
2025-12-21 23:58:37 +0100

    counting words

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def get_book_text(filepath):
     with open(filepath) as f:
         contents = f.read()
     return contents
+
+def count_words(input_text):
+    words = input_text.split()
+    return len(words)
+
 def main():
     text = get_book_text("/home/mikolaj/workspace/github.com/MD/bookbot/books/frankenstein.txt")
     print (text)
+    print(f"Found {count_words(text)} total words")
 
 main()
